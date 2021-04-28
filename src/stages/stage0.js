a6108d484a5c0f3e29d91ee5d5b46dc95be3afac
Edilson Lima <edilsonlm217@hotmail.com>
2021-04-28 15:32:45 -0400

    Criação do método execute para o stage0

--- src/stages/stage0.js
+++ src/stages/stage0.js
@@ -1,6 +1,30 @@
-function execute(user, msg, contato) {
+const { InService } = require("../database/schema/InService");
+
+async function execute(message) {
+  stamp = new Date();
+  hours = stamp.getHours();
+
+  if (hours >= 18 && hours < 24) {
+    time = "Boa noite"
+  } else if (hours >= 12 && hours < 18) {
+    time = "Boa tarde"
+  } else if (hours >= 0 && hours < 12) {
+    time = "Bom dia"
+  }
+
+  const { sender } = message;
+  const [senderId,] = sender.id.split('@c.us');
+
+  const service = await InService.findOne({ senderId });
+
+  service.stage = 1;
+  await service.save();
+
   return [
-    `Isso é uma mensagem de teste`,
+    `${time}. Seja bem vindo(a)!
+    Aqui é a assistente virtual da Updata.
+    
+    Digite *[1] para atendimento avulso* ou *informe CPF ou CNPJ* caso seja assinante de nossos serviços`,
   ];
 }
 
