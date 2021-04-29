bd05d9eb18be39ed44d29c7e38dd34289f7210a5
Edilson Lima <edilsonlm217@hotmail.com>
2021-04-29 15:37:18 -0400

    Adição de mais um estágio

--- src/stages/index.js
+++ src/stages/index.js
@@ -1,11 +1,15 @@
 var stages = {
   0: {
-    descricao: "Welcome stage",
+    descricao: "Estágio de boas vindas",
     obj: require("./stage0"),
   },
   1: {
-    descricao: "Ler resposta do usuário",
+    descricao: "Estágio de apresentação de menus",
     obj: require("./stage1"),
+  },
+  2: {
+    descricao: "Ler resposta do usuário",
+    obj: require("./stage2"),
   },
 };
 
