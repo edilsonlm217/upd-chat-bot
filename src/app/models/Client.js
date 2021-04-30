2d91c6b90697fb80c1cfe26f3b388b862e927c65
Edilson Lima <edilsonlm217@hotmail.com>
2021-04-30 19:05:13 -0400

    Criação da model de Clientes

---
+++ src/app/models/Client.js
@@ -0,0 +1,47 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Client extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+        },
+        endereco_res: Sequelize.STRING,
+        numero_res: Sequelize.STRING,
+        bairro_res: Sequelize.STRING,
+        coordenadas: Sequelize.STRING,
+        login: Sequelize.STRING,
+        senha: Sequelize.STRING,
+        plano: Sequelize.STRING,
+        tipo: Sequelize.STRING,
+        ip: Sequelize.STRING,
+        mac: Sequelize.STRING,
+        automac: Sequelize.STRING,
+        porta_olt: Sequelize.STRING,
+        caixa_herm: Sequelize.STRING,
+        ssid: Sequelize.STRING,
+        porta_splitter: Sequelize.STRING,
+        cli_ativado: Sequelize.STRING,
+        nome: Sequelize.STRING,
+        fone: Sequelize.STRING,
+        celular: Sequelize.STRING,
+        equipamento: Sequelize.STRING,
+        bloqueado: Sequelize.STRING,
+        observacao: Sequelize.STRING,
+        rem_obs: Sequelize.DATE,
+        cpf_cnpj: Sequelize.STRING,
+        status_corte: Sequelize.STRING,
+      },
+      {
+        sequelize,
+        tableName: 'sis_cliente',
+      }
+    );
+
+    return this;
+  }
+}
+
+export default Client;
