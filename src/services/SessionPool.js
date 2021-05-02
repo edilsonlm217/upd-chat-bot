363bae68eb968e75475703dcf01278d161d4e582
Edilson Lima <edilsonlm217@hotmail.com>
2021-05-02 17:19:52 -0400

    Implementação da lógica de chatbot

--- src/services/SessionPool.js
+++ src/services/SessionPool.js
@@ -1,4 +1,7 @@
 import Tenant from '../app/schema/Tenant';
+import InService from '../app/schema/InService';
+import { differenceInMinutes } from 'date-fns';
+import stages from '../stages/index';
 const venom = require('venom-bot');
 
 async function getActiveTenants() {
@@ -9,6 +12,61 @@ async function getActiveTenants() {
   return tenants;
 }
 
+async function getOnGoingService(senderId) {
+  const service = await InService.findOne({ senderId });
+  return service;
+}
+
+async function getStage(message) {
+  const { sender } = message;
+  const [senderId,] = sender.id.split('@c.us');
+
+  const service = await getOnGoingService(senderId);
+
+  if (service) {
+    const { lastMessageReceivedAt } = service;
+
+    if (differenceInMinutes(new Date(), lastMessageReceivedAt) > 15) {
+      console.log('[LOG]: Atendimento expirado');
+      service.remove();
+    } else {
+      console.log('[LOG]: Atendimento encontrado');
+      return service.stage;
+    }
+  }
+
+  console.log('[LOG]: Iniciando novo atendimento');
+
+  // Inicia um novo atendimento
+  const newService = await InService.create({
+    // TODO: Criar um número de protocolo significativo
+    protocolNumber: '12456489712345',
+    senderId,
+    lastMessageReceivedAt: new Date(),
+  });
+
+  return newService.stage;
+}
+
+async function startBot(client) {
+  client.onMessage(async message => {
+    if (message.isGroupMsg === false) {
+      const stage = await getStage(message);
+
+      try {
+        const resp = await stages[stage].obj.execute(message);
+
+        for (let index = 0; index < resp.length; index++) {
+          const element = resp[index];
+          client.sendText(message.from, element);
+        }
+      } catch (error) {
+        console.log(`[LOG]: ! ${error}`);
+      }
+    }
+  });
+}
+
 class SessionPool {
   constructor() {
     this.sessions = { /* sessionName: session */ };
@@ -16,7 +74,6 @@ class SessionPool {
 
   add(client) {
     this.sessions[client.session] = client;
-    console.log(this.sessions);
   }
 
   preloadSessions() {
@@ -40,9 +97,10 @@ class SessionPool {
             this.sessions[client.session] = client;
             console.log(`[LOG]: ${sessionName}'s session has been preloaded`);
             //
+            startBot(client);
           }
         ).catch(error => {
-          console.log(error);
+          console.log(`[LOG]: ${error}`);
         });
 
       });
