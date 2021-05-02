d3a94db091aed50794d30bab43f17b3a2e0755f3
Edilson Lima <edilsonlm217@hotmail.com>
2021-05-01 21:17:23 -0400

    Alteração da url de acesso ao código qr code

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,11 @@
 import { Router } from 'express';
 
 import TenantController from './app/controllers/TenantController';
+import WASessionController from './app/controllers/WhatsappSessionController';
 
 const routes = new Router();
 
 routes.post('/add/tenant', TenantController.create);
+routes.get('/:sessionName/scan', WASessionController.create);
 
 export default routes;
