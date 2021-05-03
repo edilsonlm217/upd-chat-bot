5e174ff1b2050ae562ec9e7b35c786688fcc0b96
Edilson Lima <edilsonlm217@hotmail.com>
2021-05-03 17:34:01 -0400

    Implementação estrutura try catch

--- src/app/controllers/TenantController.js
+++ src/app/controllers/TenantController.js
@@ -2,22 +2,26 @@ import Tenant from '../schema/Tenant';
 
 class TenantController {
   async create(req, res) {
-    const { nome, cnpj } = req.body;
-    const { dialect, host, username, password, database, port, sessionName } = req.body;
+    try {
+      const { nome, cnpj } = req.body;
+      const { dialect, host, username, password, database, port, sessionName } = req.body;
 
-    const tenant = await Tenant.create({
-      nome,
-      cnpj,
-      dialect,
-      host,
-      port,
-      username,
-      password,
-      database,
-      sessionName,
-    });
+      const tenant = await Tenant.create({
+        nome,
+        cnpj,
+        dialect,
+        host,
+        port,
+        username,
+        password,
+        database,
+        sessionName,
+      });
 
-    return res.json(tenant);
+      return res.json(tenant);
+    } catch (error) {
+      console.log('[ERROR]: Failed to create tenant');
+    }
   }
 }
 
