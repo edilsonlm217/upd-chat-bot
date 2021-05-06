af61b3b03ea208256580f7f301fd6aa99d37b2d4
Edilson Lima <edilsonlm217@hotmail.com>
2021-05-06 04:05:38 -0400

    Criação do stage 3

--- src/chatbot/stages/stage3.js
+++ src/chatbot/stages/stage3.js
@@ -1,15 +1,53 @@
 import InService from '../../app/schema/InService';
 
-const attachedResponses = {
-
+const financeResponses = {
+  '1': 'Segunda via de fatura',
+  '2': 'Desbloqueio de confiança',
+  '3': 'Cancelamento',
+  '4': 'Falar com atendente',
+  '0': 'Menu principal',
+  '#': 'Finalizar atendimento',
 };
 
-const detachedResponses = {
-
+const supportResponses = {
+  '1': 'Sem internet',
+  '2': 'Internet lenta',
+  '3': 'Senha do wifi',
+  '4': 'Consultar chamado',
+  '5': 'Falar com atendente',
+  '0': 'Menu principal',
+  '#': 'Finalizar atendimento',
 };
+
+async function handleResponse(message, menuStage) {
+  var response = undefined;
+
+  switch (menuStage) {
+    case 'Financeiro':
+      response = attachedResponses[message.body];
+
+      if (!response) {
+        return 'Opção inválida';
+      }
+
+      return response;
+
+    case 'detached':
+      response = detachedResponses[message.body];
+
+      if (!response) {
+        return 'Opção inválida';
+      }
+
+      return response;
+  }
+}
 
 async function execute(message) {
-  return ['Estágio 3'];
+  const { service, menuStage } = await getServiceType(message);
+  const selectedOption = await handleResponse(message, menuStage);
+
+
 }
 
 exports.execute = execute;
