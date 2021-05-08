ff64f574c6a3b2c8f8f9b7049b68540a0221a9e9
Edilson Lima <edilsonlm217@hotmail.com>
2021-05-08 13:36:05 -0400

    Criação de nova rota

--- src/routes.js
+++ src/routes.js
@@ -2,10 +2,12 @@ import { Router } from 'express';
 
 import TenantController from './app/controllers/TenantController';
 import WASessionController from './app/controllers/WhatsappSessionController';
+import MessengerController from './app/controllers/MessengerController';
 
 const routes = new Router();
 
 routes.post('/add/tenant', TenantController.create);
 routes.get('/:sessionName/scan', WASessionController.create);
+routes.post('/whatsapp', MessengerController.store);
 
 export default routes;
