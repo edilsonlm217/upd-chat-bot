f0ca68f9f6a6cb6f2bbdff4b53063e9a947f03bd
Edilson Lima <edilsonlm217@hotmail.com>
2021-05-14 09:08:33 -0400

    Implementação de mais 2 novos estagios

--- src/chatbot/stages/index.js
+++ src/chatbot/stages/index.js
@@ -1,5 +1,7 @@
 import WelcomeStage from './WelcomeStage/index';
 import IdentificationStage from './IdentificationStage/index';
+import SelectAreaAsClient from './SelectAreaAsClient/index';
+import CompletionStage from './CompletionStage/index';
 
 exports.stages = {
   "welcome": {
@@ -10,13 +12,12 @@ exports.stages = {
     execute: async (attndnce, message) => await IdentificationStage(attndnce, message),
     description: 'Identify whether the contact is a customer and present the menu equivalent to the response',
   },
-  "non_client": {
-    execute: () => { return 'non_client' }
-  },
   "selecting_area_as_client": {
-    execute: () => { return 'selecting_area_as_client' }
+    execute: async (attndnce, message) => await SelectAreaAsClient(attndnce, message),
+    description: 'Internship for ISP customers only. Identifies which menu the user is trying to access',
   },
-  "non_client_1": {
-    execute: () => { return 'non_client_1' }
+  "completion": {
+    execute: async (attndnce, message) => await CompletionStage(attndnce, message),
+    description: 'Responsible for validating if the user wants to do anything or finish the service',
   },
 }
