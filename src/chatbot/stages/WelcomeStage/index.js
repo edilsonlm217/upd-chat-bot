6b793197025a40676135143bf7f13eeccbaa19b8
Edilson Lima <edilsonlm217@hotmail.com>
2021-05-14 08:39:41 -0400

    Implementação de try catch

--- src/chatbot/stages/WelcomeStage/index.js
+++ src/chatbot/stages/WelcomeStage/index.js
@@ -1,6 +1,11 @@
 export default async function WelcomeStage(attndnce, message) {
-  attndnce.stage = 'identification';
-  await attndnce.save();
+  try {
+    attndnce.stage = 'identification';
+    await attndnce.save();
 
-  return ['Bem vindo!\nPra começar informe seu CPF ou CNPJ.\nSe ainda não é cliente digite [1]'];
+    return ['Bem vindo!\nPra começar informe seu CPF ou CNPJ.\nSe ainda não é cliente digite [1]'];
+  } catch (error) {
+    console.log('[LOG]: Failed @ WelcomeStage');
+    console.log(error);
+  }
 }
