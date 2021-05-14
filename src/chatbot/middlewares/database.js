84d295789ac939e095f0eaf9ed85deb6355edc1f
Edilson Lima <edilsonlm217@hotmail.com>
2021-05-14 15:10:58 -0400

    Adição da model Invoice

--- src/chatbot/middlewares/database.js
+++ src/chatbot/middlewares/database.js
@@ -1,8 +1,9 @@
 import Database from '../database/';
 
 import Client from '../../app/models/Client';
+import Invoice from '../../app/models/Invoice';
 
-const models = [Client];
+const models = [Client, Invoice];
 
 export default function databaseToggler(sessionName) {
   const sqlConnection = Database.connections[sessionName];
