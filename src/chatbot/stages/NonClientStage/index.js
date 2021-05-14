b38b5b8df17ce90be68311404a90f23366461308
Edilson Lima <edilsonlm217@hotmail.com>
2021-05-14 09:13:58 -0400

    Criação de um novo estágio

---
+++ src/chatbot/stages/NonClientStage/index.js
@@ -0,0 +1,8 @@
+export default async function NonClientStage(attndnce, message) {
+  try {
+    //
+  } catch (error) {
+    console.log('[LOG]: Failed @ NonClientStage');
+    console.log(error);
+  }
+}
