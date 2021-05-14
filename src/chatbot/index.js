9f48cc97a36bec0d8ca82cd2927b301a9fe9dd43
Edilson Lima <edilsonlm217@hotmail.com>
2021-05-14 08:29:16 -0400

    Modificação do método getStage para getAttendance

--- src/chatbot/index.js
+++ src/chatbot/index.js
@@ -3,12 +3,13 @@ import { differenceInMinutes } from 'date-fns';
 import fs from 'fs';
 
 import Tenant from '../app/schema/Tenant';
-import InService from '../app/schema/InService';
+import Attendance from '../app/schema/Attendance';
+
+import { stages } from './stages/index';
 
 export default new function Chatbot() {
   const middlewares = {};
   const sessions = {};
-  const atendimentos = {};
 
   function constructor() { }
 
@@ -34,216 +35,15 @@ export default new function Chatbot() {
       sessions[sessionName] = client;
 
       client.onMessage(async message => {
-        await runMiddlewares();
+        await runMiddlewares(sessionName);
 
         if (message.from === '559236483445@c.us') {
-          var atendimento = atendimentos[message.from];
+          const attndnce = await getAttendance(message, sessionName);
+          const response = await stages[attndnce.stage].execute(attndnce, message);
 
-          // area: welcome
-          if (!atendimento) {
-            client.sendText(message.from, 'Bem vindo!\nPra começar informe seu CPF ou CNPJ.\nSe ainda não é cliente digite [1]');
-
-            atendimentos[message.from] = {
-              area: 'identification',
-              action: 'handleResponse',
-              isClient: null,
-              controle: null,
-            };
-
-            return;
-          }
-
-          // area: identification
-          // action: handleResponse
-          if (atendimento.area === 'identification' && atendimento.action === 'handleResponse') {
-            // Verifica se resposta contém um CPF/CNPJ válido
-            const regex = /([0-9]{2}[\.]?[0-9]{3}[\.]?[0-9]{3}[\/]?[0-9]{4}[-]?[0-9]{2})|([0-9]{3}[\.]?[0-9]{3}[\.]?[0-9]{3}[-]?[0-9]{2})/;
-
-            const isValidCPFCNPJ = regex.test(message.body);
-
-            if (isValidCPFCNPJ) {
-              atendimento.area = 'client_area';
-              atendimento.action = 'handleResponse';
-              atendimento.isClient = true;
-              atendimento.controle = 'client';
-
-              client.sendText(message.from, 'Para qual desses assuntos deseja atendimento:\n\n1. Financeiro\n2. Suporte\n');
-
-              atendimentos[message.from] = atendimento;
-              return;
-            }
-
-            // Verifica se resposta solicita atendimento avulso
-            if (message.body == '1') {
-              atendimento.area = 'non_client_area';
-              atendimento.action = 'handleResponse';
-              atendimento.isClient = false;
-              atendimento.controle = 'non_client';
-
-              client.sendText(message.from, 'Para qual desses assuntos deseja atendimento:\n\n1. Conhecer planos\n2. Falar com atendente');
-
-              atendimentos[message.from] = atendimento;
-              return;
-            }
-
-            client.sendText(message.from, 'Opção inválida');
-            return;
-          }
-
-          // area: client_area
-          // action: handleResponse
-          if (atendimento.area === 'client_area' && atendimento.action === 'handleResponse') {
-            switch (message.body) {
-              case '1':
-                // Financeiro
-                atendimento.area = 'financeiro';
-                atendimento.action = 'handleResponse';
-                atendimento.isClient = true;
-                atendimento.controle = 'client.financeiro';
-
-                await client.sendText(message.from, '*Financeiro*\n1. Segunda via de fatura\n2. Desbloqueio de confiança');
-                await client.sendText(message.from, '0. Voltar');
-
-                atendimentos[message.from] = atendimento;
-                break;
-
-              case '2':
-                // Suporte
-                atendimento.area = 'suporte';
-                atendimento.action = 'handleResponse';
-                atendimento.isClient = true;
-                atendimento.controle = 'client.suporte';
-
-                await client.sendText(message.from, '*Suporte*\n1. Sem internet\n2. Internet lenta\n3. Consultar chamado');
-                await client.sendText(message.from, '0. Voltar');
-
-                atendimentos[message.from] = atendimento;
-                console.log(atendimentos[message.from]);
-                break;
-
-              default:
-                // Opção inválida
-                client.sendText(message.from, 'Opção inválida');
-                console.log(atendimentos[message.from]);
-                break;
-            }
-
-            // Interromper onMessage();
-            return;
-          }
-
-          // area: non_client_area
-          // action: handleResponse
-          if (atendimento.area === 'non_client_area' && atendimento.action === 'handleResponse') {
-            if (message.body === '1') {
-              // Conhecer planos
-              await client.sendText(message.from, '*Temos o plano ideal para você:*\n\n*PLANOS COM CABO*\nBásico 5 Megas: R$ 50,00 / mês\nEconômico 10 Megas: R$ 70,00 / mês\n\n*PLANOS COM FIBRA*\nPadrão 20 Megas: R$ 100,00 / mês\nProfissional 30 Megas: R$ 120,00 / mês\nSmart 50 Megas: R$ 150,00 / mês\nTopFibra 100 Megas: R$ 200,00 / mês\n\n- Taxa de ativação R$ 100,00\n- Roteador* R$ 100,00\n\n(*) nos planos a partir de 50 megas WIFI grátis com roteador em comodato\n\nPara contratar acesse:\n*www.updata.com.br/planos*');
-              await client.sendText(message.from, 'Se era somente isso, você pode:\n0. Voltar\n#. Finalizar atendimento');
-
-              atendimento.area = 'non_client_area';
-              atendimento.action = 'goback';
-
-              atendimentos[message.from] = atendimento;
-              return;
-            }
-
-            // Opção inválida
-            client.sendText(message.from, 'Opção inválida');
-            return;
-          }
-
-          // area: financeiro;
-          // action: handleResponse
-          if (atendimento.area === 'financeiro' && atendimento.action === 'handleResponse') {
-            if (message.body == '1') {
-              // Segunda via de fatura
-              client.sendText(message.from, 'www.updata/fatura/fulano.pdf');
-              return;
-            }
-
-            if (message.body == '2') {
-              // Desbloquear internet
-              client.sendText(message.from, 'Internet desbloqueada até dia 22/07/1992');
-              return;
-            }
-
-            if (message.body == '0') {
-              // Voltar
-              atendimento.area = 'client_area';
-              atendimento.action = 'handleResponse';
-              atendimento.isClient = true;
-              atendimento.controle = 'client';
-
-              client.sendText(message.from, 'Para qual desses assuntos deseja atendimento:\n\n1. Financeiro\n2. Suporte\n');
-              return;
-            }
-
-            // Opção inválida
-            client.sendText(message.from, 'Opção inválida');
-            return;
-          }
-
-          // area: suporte;
-          // action: handleResponse
-          if (atendimento.area === 'suporte' && atendimento.action === 'handleResponse') {
-            if (message.body == '1') {
-              // Sem internet
-              client.sendText(message.from, 'Verificamos aqui e você possui internet');
-              return;
-            }
-
-            if (message.body == '2') {
-              // Internet lenta
-              client.sendText(message.from, 'Verificamos aqui e sua internet não apresenta lentidão');
-              return;
-            }
-
-            if (message.body == '3') {
-              // Consultar chamado
-              client.sendText(message.from, 'Verificamos que não há chamado aberto para você');
-              return;
-            }
-
-            if (message.body == '0') {
-              // Voltar
-              atendimento.area = 'client_area';
-              atendimento.action = 'handleResponse';
-              atendimento.isClient = true;
-              atendimento.controle = 'client';
-
-              client.sendText(message.from, 'Para qual desses assuntos deseja atendimento:\n\n1. Financeiro\n2. Suporte\n');
-              return;
-            }
-
-            // Opção inválida
-            client.sendText(message.from, 'Opção inválida');
-            return;
-          }
-
-          // area: non_client_area
-          // action: goback
-          if (atendimento.area === 'non_client_area' && atendimento.action === 'goback') {
-            if (message.body == '0') {
-              // Voltar
-              atendimento.area = 'non_client_area';
-              atendimento.action = 'handleResponse';
-              atendimento.isClient = false;
-              atendimento.controle = 'non_client';
-
-              client.sendText(message.from, 'Para qual desses assuntos deseja atendimento:\n\n1. Conhecer planos\n2. Falar com atendente');
-
-              atendimentos[message.from] = atendimento;
-              return;
-            }
-
-            if (message.body == '#') {
-              // Finalizar atendimento
-              client.sendText(message.from, 'Atendimento finalizado');
-            }
-          }
-
-          // Não deu match com nenhuma opção
-          console.log('Não entrei em nada');
+          response.map(async msg => {
+            await client.sendText(message.from, msg);
+          });
         }
       });
     } catch (error) {
@@ -251,35 +51,34 @@ export default new function Chatbot() {
     }
   }
 
-  async function getStage(message, sessionName) {
+  async function getAttendance(message, sessionName) {
     try {
       const { sender } = message;
-      const [senderId,] = sender.id.split('@c.us');
 
-      const service = await InService.findOne({ senderId });
+      const attendance = await Attendance.findOne({ senderId: sender.id });
 
-      if (service) {
-        const { lastMessageReceivedAt } = service;
+      if (attendance) {
+        const { lastMessageReceivedAt } = attendance;
 
         if (differenceInMinutes(new Date(), lastMessageReceivedAt) > 15) {
           console.log('[LOG]: Atendimento expirado');
-          service.remove();
+          attendance.remove();
         } else {
           console.log('[LOG]: Atendimento encontrado');
-          return service.stage;
+          return attendance;
         }
       }
 
       console.log('[LOG]: Iniciando novo atendimento');
 
       // Inicia um novo atendimento
-      const newService = await InService.create({
-        senderId,
+      const newAttendance = await Attendance.create({
+        senderId: sender.id,
         sessionName,
         lastMessageReceivedAt: new Date(),
       });
 
-      return newService.stage;
+      return newAttendance;
     } catch (error) {
       console.log('[LOG]: Failed to get conversation stage');
     }
@@ -301,7 +100,7 @@ export default new function Chatbot() {
               return;
             }
 
-            listenMessages(tenant.sessionName);
+            await listenMessages(tenant.sessionName);
 
           });
         }
@@ -317,7 +116,7 @@ export default new function Chatbot() {
     sessions: sessions,
     middlewares: middlewares,
     init: () => { reload() },
-    listenMessages: (sessionName) => { listenMessages(sessionName) },
+    listenMessages: async (sessionName) => { await listenMessages(sessionName) },
     use: (funcName, func) => { use(funcName, func) }
   }
 
