6f8dc6f8c9730e3eb6ff0504df0e792545f0beab
Edilson Lima <edilsonlm217@hotmail.com>
2021-05-14 15:11:46 -0400

    Adição do atributo linhadig

---
+++ src/app/models/Invoice.js
@@ -0,0 +1,31 @@
+import Sequelize, { Model } from 'sequelize';
+
+class Invoice extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        id: {
+          type: Sequelize.INTEGER,
+          primaryKey: true,
+        },
+        datavenc: Sequelize.DATE,
+        datapag: Sequelize.DATE,
+        linhadig: Sequelize.STRING,
+        datadel: Sequelize.DATE,
+        valor: Sequelize.STRING,
+        status: Sequelize.STRING,
+        login: Sequelize.STRING,
+        tipo: Sequelize.STRING,
+        obs: Sequelize.STRING,
+      },
+      {
+        sequelize,
+        tableName: 'sis_lanc',
+      }
+    );
+
+    return this;
+  }
+}
+
+export default Invoice;
