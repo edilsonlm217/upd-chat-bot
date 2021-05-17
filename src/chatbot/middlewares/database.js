a7ac0f1a2cba0ed5ed40c9bc4ef9a35384844161
Edilson Lima <edilsonlm217@hotmail.com>
2021-05-17 19:57:59 -0400

    Adição de duas novas models ao app

--- src/chatbot/middlewares/database.js
+++ src/chatbot/middlewares/database.js
@@ -2,11 +2,15 @@ import Database from '../database/';
 
 import Client from '../../app/models/Client';
 import Invoice from '../../app/models/Invoice';
+import OnlineUsers from '../../app/models/OnlineUsers';
+import SupportRequest from '../../app/models/SupportRequest';
 
-const models = [Client, Invoice];
+const models = [Client, Invoice, OnlineUsers, SupportRequest];
 
 export default function databaseToggler(sessionName) {
   const sqlConnection = Database.connections[sessionName];
+
   models.map(model => model.init(sqlConnection));
+
   return true;
 }
