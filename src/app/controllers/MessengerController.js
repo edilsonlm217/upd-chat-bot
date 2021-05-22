5f6ad6c5d80f55c40e5c2d7c844a6a2e613cbcc9
Edilson Lima <edilsonlm217@hotmail.com>
2021-05-21 21:19:09 -0400

    Implementação da lógica de envio de PDF

--- src/app/controllers/MessengerController.js
+++ src/app/controllers/MessengerController.js
@@ -1,4 +1,8 @@
 import Chatbot from '../../chatbot';
+import pdf from 'html-pdf';
+// import Buffer from 'buffer';
+import iconv from 'iconv-lite';
+import request from 'request';
 import { charset, fixAccentuation } from '../../utils';
 
 class MessengerController {
@@ -22,10 +26,31 @@ class MessengerController {
       } while (formattedString.includes(value));
     }
 
-    Chatbot.sessions['updata'].sendText(
-      `${number}@c.us`,
-      formattedString
-    );
+    const html = 'http://provedor.updata.com.br/boleto/20boleto.php?titulo=70559';
+
+    request({ uri: html, encoding: null }, (error, response, body) => {
+      const utf8String = iconv.decode(new Buffer(body), "ISO-8859-1");
+      console.log(utf8String);
+      pdf.create(utf8String).toFile('./tmp.pdf', async (err, res) => {
+        console.log(res.filename);
+
+        try {
+          await Chatbot.sessions['updata'].sendText(
+            `${number}@c.us`,
+            message,
+          );
+
+          await Chatbot.sessions['updata'].sendFile(
+            `${number}@c.us`,
+            res.filename
+          );
+        } catch (error) {
+          console.log(error);
+        }
+      });
+    });
+
+    return res.json({ ok: true });
 
     res.json({
       message: formattedString,
