5de3a429780569d553e6b70182a2c0579ad16772
Edilson Lima <edilsonlm217@hotmail.com>
2021-05-24 11:06:15 -0400

    Implementação para enviar boleto via PDF via wpp

--- src/app/controllers/WhatsappSessionController.js
+++ src/app/controllers/WhatsappSessionController.js
@@ -1,6 +1,8 @@
 import Tenant from '../schema/Tenant';
 import Chatbot from '../../chatbot';
 
+const bot = require('venom-bot');
+
 class WhatsappSessionController {
   async create(req, res) {
     try {
@@ -15,11 +17,35 @@ class WhatsappSessionController {
         return;
       }
 
-      // TODO Implementar inicialização do socket.io para envio
-      Chatbot.listenMessages(tenant.sessionName);
+      const client = await bot.create(
+        sessionName,
+        (base64Qr, asciiQR, attempts, urlCode) => { console.log('haahha'); },
+        undefined,
+        {
+          logQR: true,
+          disableWelcome: true,
+          autoClose: 30000,
+        }
+      );
+
+      Chatbot.sessions[sessionName] = client;
+
+      client.onMessage(async message => {
+        await runMiddlewares(sessionName);
+
+        if (message.from === '559236483445@c.us') {
+          const attndnce = await getAttendance(message, sessionName);
+          const response = await stages[attndnce.stage].execute(attndnce, message);
+
+          for (let msg of response) {
+            await client.sendText(message.from, msg);
+          }
+        }
+      });
 
       return res.json({ ok: true });
     } catch (error) {
+      console.log(error);
       console.log('[LOG]: Failed to create Whatsapp session');
     }
   }
