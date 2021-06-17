cfada96a1316237ecbb0f5df912812b172373053
Edilson Lima <edilsonlm217@hotmail.com>
2021-06-16 23:14:48 -0400

    Criação de novas rotas

--- src/routes.js
+++ src/routes.js
@@ -1,13 +1,25 @@
 import { Router } from 'express';
 
-import TenantController from './app/controllers/TenantController';
 import WASessionController from './app/controllers/WhatsappSessionController';
 import MessengerController from './app/controllers/MessengerController';
 
+import AccountController from './app/controllers/panel/AccountController';
+import SessionController from './app/controllers/panel/SessionController';
+import ChatbotMessagesController from './app/controllers/panel/ChatbotMessagesController';
+import VenomSessionController from './app/controllers/panel/VenomSessionController';
+
+import VenomBotMiddleware from './app/middlewares/venombot';
+
 const routes = new Router();
 
-routes.post('/add/tenant', TenantController.create);
+routes.post('/account', AccountController.store);
+routes.post('/session', SessionController.store);
+routes.get('/venom/session/:userId', VenomSessionController.show);
+routes.post('/venom/session/:userId', VenomBotMiddleware, VenomSessionController.update);
+
 routes.get('/:sessionName/scan/:socketId', WASessionController.create);
 routes.post('/whatsapp', MessengerController.store);
+routes.get('/messages/:ownerId', ChatbotMessagesController.index);
+routes.put('/messages/:ownerId/:msgId', ChatbotMessagesController.update);
 
 export default routes;
