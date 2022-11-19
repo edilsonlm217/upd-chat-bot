952de6127af9a4d87ff587b05f6617c759f93639
Edilson Lima <edilsonlm217@hotmail.com>
2022-11-19 18:30:55 -0400

    Remoção de rotas desnecessárias

--- src/routes.js
+++ src/routes.js
@@ -1,14 +1,8 @@
 import { Router } from 'express';
-import MessengerController from './app/controllers/MessengerController';
 
-import WASessionController from './app/controllers/WhatsappSessionController';
 import MessengerController from './app/controllers/MessengerController';
 
 const routes = new Router();
-
-routes.post('/session/store', WASessionController.store);
-routes.post('/session/update', WASessionController.update);
-routes.post('/session/destroy', WASessionController.destroy);
 
 routes.post('/message', MessengerController.store);
 
