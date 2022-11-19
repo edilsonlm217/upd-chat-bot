b4b6c1c1c8c4c4d6fe8946c9ef4b878eb2aa34ca
Edilson Lima <edilsonlm217@hotmail.com>
2022-11-19 18:30:40 -0400

    Alteração do service websocket

--- src/app.js
+++ src/app.js
@@ -3,6 +3,8 @@ import cors from 'cors';
 import http from 'http';
 import routes from './routes';
 
+import Websocket from './services/Websocket';
+
 class App {
   constructor() {
     this.app = express();
@@ -10,6 +12,7 @@ class App {
 
     this.middlewares();
     this.routes();
+    this.socket();
   }
 
   middlewares() {
@@ -19,6 +22,10 @@ class App {
 
   routes() {
     this.app.use(routes);
+  }
+
+  socket() {
+    Websocket.start(this.server);
   }
 }
 
