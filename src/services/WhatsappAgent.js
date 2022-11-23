2e725a034657eab203a5d902b0e711955695f98a
Edilson Lima <edilsonlm217@hotmail.com>
2022-11-23 09:13:51 -0400

    Remoção de console.log

--- src/services/WhatsappAgent.js
+++ src/services/WhatsappAgent.js
@@ -2,7 +2,6 @@ const sessions = {};
 
 function registerWhatsappAgent(venomSession, client) {
     sessions[client] = venomSession;
-    console.log(sessions);
 }
 
 function getSessionByClientAccount(client) {
