3ccbdb44290dfa185add0e319596d7e4c45c533c
Edilson Lima <edilsonlm217@hotmail.com>
2022-11-28 22:29:40 -0400

    Adição de rotas

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,18 @@
 import { Router } from 'express';
 
+import AuthController from './app/controllers/AuthController';
+import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 import MessengerController from './app/controllers/MessengerController';
 
 const routes = new Router();
 
 routes.post('/message', MessengerController.store);
+
+routes.get('/session/:sessionName', SessionController.show);
+
+routes.post('/auth', AuthController.index);
+
+routes.post('/user', UserController.store);
 
 export default routes;
