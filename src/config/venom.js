f3c0660ea7e0582230208f0ce8248a427ee8c6ea
Edilson Lima <edilsonlm217@hotmail.com>
2022-12-02 11:49:36 -0400

    Atualização das definições do venom-bot

--- src/config/venom.js
+++ src/config/venom.js
@@ -1,8 +1,8 @@
 export default {
     multidevice: false, // for version not multidevice use false.(default: true)
     folderNameToken: 'tokens', //folder name when saving tokens
-    mkdirFolderToken: '/node_modules', //folder directory tokens, just inside the venom folder, example:  { mkdirFolderToken: '/node_modules', } //will save the tokens folder in the node_modules directory
-    headless: true, // Headless chrome
+    mkdirFolderToken: '', //folder directory tokens, just inside the venom folder, example:  { mkdirFolderToken: '/node_modules', } //will save the tokens folder in the node_modules directory
+    headless: false, // Headless chrome
     devtools: false, // Open devtools by default
     useChrome: true, // If false will use Chromium instance
     debug: false, // Opens a debug session
@@ -11,8 +11,8 @@ export default {
     browserArgs: [''], // Original parameters  ---Parameters to be added into the chrome browser instance
     addBrowserArgs: [''], // Add broserArgs without overwriting the project's original
     puppeteerOptions: {}, // Will be passed to puppeteer.launch
-    disableSpins: false, // Will disable Spinnies animation, useful for containers (docker) for a better log
-    disableWelcome: false, // Will disable the welcoming message which appears in the beginning
+    disableSpins: true, // Will disable Spinnies animation, useful for containers (docker) for a better log
+    disableWelcome: true, // Will disable the welcoming message which appears in the beginning
     updatesLog: true, // Logs info updates automatically in terminal
     autoClose: 60000, // Automatically closes the venom-bot only when scanning the QR code (default 60 seconds, if you want to turn it off, assign 0 or false)
     createPathFileToken: false, // creates a folder when inserting an object in the client's browser, to work it is necessary to pass the parameters in the function create browserSessionToken
